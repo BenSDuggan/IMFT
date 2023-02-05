b36e059c20c5372a19c7fa40ed6260deeab26d87
BenSDuggan <dugganbens@gmail.com>
2023-02-05 17:59:21 -0500

    Updating prod

--- server/prod.js
+++ server/prod.js
@@ -1,23 +1,32 @@
+// Node entrypoint for production
 
-process.env.IMFT_ENV = "developer";
+const utils = require('./src/utils.js')
 
-var {database} = require('./src/database.js')
-var { } = require('./src/sockets.js')
-let { logger } = require('./src/logger.js')
-let { } = require('./src/twitter.js');
-const config = require('./src/config.js')
+process.env.IMFT_ENV = utils.config.env ?? "development";
+process.env.IMFT_VERSION = utils.config.version ?? "vUnknown"
+
+const { logger } = require('./src/logger.js')
 const adsb = require('./src/adsb.js')
+const {database} = require('./src/database.js')
+var { } = require('./src/sockets.js');
+const { twitter } = require('./src/twitter.js');
 
 
 logger.info(process.env.IMFT_VERSION + ": " + process.env.IMFT_ENV)
 logger.info("Production server started")
 
 // Detect if server is exiting from keyboard interrupt
-process.on('SIGINT', function() {
-  logger.info("Production server stopping");
+process.on('SIGINT', async () => {
+  logger.info("Server stopping...");
+  await database.disconnect();
+
+  logger.info("Server stopped");
 
   process.exit();
 });
 
+twitter.read_only = true;;
+
 adsb.receiver = new adsb.OpenSky();
 adsb.receiver.start();
+
